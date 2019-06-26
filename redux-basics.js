ccd5fa9cdb67f43da3337273df1919c15adea018
Steve Doggett <artigat1@users.noreply.github.com>
2019-06-26 08:54:23 +0100

    Dispatching actions
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/lecture/8211840#overview

--- redux-basics.js
+++ redux-basics.js
@@ -2,12 +2,32 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-    counter: 0    
+    counter: 0
+};
+
+const types = {
+    incCounter: 'INC_COUNTER',
+    addCounter: 'ADD_COUNTER'
 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-    return state;
+    switch(action.type) {
+        case types.incCounter:
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+            
+        case types.addCounter:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+            
+        default:
+            return state;
+    }
 };
 
 // Store
@@ -15,6 +35,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
-
+store.dispatch({type: types.incCounter});
+store.dispatch({type: types.addCounter, value: 10});
+console.log(store.getState());
 
 // Subscription
